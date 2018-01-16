78508b3808ae739c8f8c92c3a66d5d6f621bb4d0
ゆうきゃん <35098175+ucan-lab@users.noreply.github.com>
2018-01-16 23:56:43 +0900

    サイドバー カレンダーデザイン

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,14 +1,4 @@
 <div class="sidebar">
-    <div class="card card-gray">
-        <div class="card-header">
-            <h2>カテゴリ一覧</h2>
-        </div>
-        <div class="card-content">
-            <ul class="categories">
-                @foreach ($categories as $category)
-                    <li>{{ $category->name }}</li>
-                @endforeach
-            </ul>
-        </div>
-    </div>
+    @include("components.sidebar.category")
+    @include("components.sidebar.calendar")
 </div>
---
+++ resources/views/components/sidebar/calendar.blade.php
@@ -0,0 +1,70 @@
+<div class="card">
+    <div class="card-header">
+        <h2>カレンダー</h2>
+    </div>
+    <div class="card-content">
+        <div class="calendar">
+            <div class="control">
+                <div><<</div>
+                <div>2018年1月</div>
+                <div>>></div>
+            </div>
+            <div class="month">
+                <div class="week">
+                    <div class="day day-sunday">日</div>
+                    <div class="day">月</div>
+                    <div class="day">火</div>
+                    <div class="day">水</div>
+                    <div class="day">木</div>
+                    <div class="day">金</div>
+                    <div class="day day-saturday">土</div>
+                </div>
+                <div class="week">
+                    <span class="day"></span>
+                    <span class="day"></span>
+                    <span class="day"></span>
+                    <span class="day">1</span>
+                    <span class="day">2</span>
+                    <a href="#" class="day">3</a>
+                    <span class="day">4</span>
+                </div>
+                <div class="week">
+                    <span class="day">5</span>
+                    <span class="day">6</span>
+                    <span class="day">7</span>
+                    <span class="day">8</span>
+                    <span class="day">9</span>
+                    <span class="day">10</span>
+                    <span class="day">11</span>
+                </div>
+                <div class="week">
+                    <span class="day">12</span>
+                    <span class="day">13</span>
+                    <span class="day">14</span>
+                    <span class="day">15</span>
+                    <span class="day">16</span>
+                    <span class="day">17</span>
+                    <span class="day">18</span>
+                </div>
+                <div class="week">
+                    <span class="day">19</span>
+                    <span class="day">20</span>
+                    <span class="day">21</span>
+                    <span class="day">22</span>
+                    <span class="day">23</span>
+                    <span class="day">24</span>
+                    <span class="day">25</span>
+                </div>
+                <div class="week">
+                    <span class="day">26</span>
+                    <span class="day">27</span>
+                    <span class="day">28</span>
+                    <span class="day">29</span>
+                    <span class="day">30</span>
+                    <span class="day">31</span>
+                    <span class="day"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
